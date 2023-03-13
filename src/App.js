678f5134d6f30da396f9e439d6dffd922bf5b11d
BushraFatimaBF <vglampard@protonmail.com>
2023-03-13 15:38:33 +0000

    merged linting fixes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import logo from './logo.png';
 import { Counter } from './features/counter/Counter';
 import './App.css';
-import Result from './features/result/Result';
 
 
 function App() {
   return (
     <div className="App" >
       <header className="App-header" >
-        <img src={logo} className="App-logo" alt="logo" />
+    
         <Counter />
-        <Result/>
+ 
       </header>
     </div>
   );
